e0492ba981fcc99a4daf7712e3acfc551014e190
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-10 15:15:18 +0700

    membahkan method peek

--- stackusingsingly.cpp
+++ stackusingsingly.cpp
@@ -49,6 +49,21 @@ class stack{
         cout << "Popped value: " << top->data << endl;
         top = top->next; // Update the top pointer to the next node
     }
+
+    // Peek/Top operation: Retrieve the value of the topmost element without removing it
+    void peek() {
+        if (top == NULL) {
+            cout << "List is empty" << endl;
+        }
+        else {
+            Node* current = top;
+            while (current != NULL) {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }// return the value of the top node
+    }
     
 };
 
