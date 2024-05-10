f32dbdd1c4882efed560fed1a1d45c415062bcff
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-10 14:56:16 +0700

    menambahkan method push didalam class stack

--- stackusingsingly.cpp
+++ stackusingsingly.cpp
@@ -23,4 +23,14 @@ class stack{
     stack(){
         top = NULL ; // initialize the stack with null top pointer
     }
+
+    // Push operation: Insert an element onto the top of the stack
+    int push(int value) {
+        Node* newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. Assign value
+        newNode->next = top; // 3. Set the next pointer of the new node to the current top node
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
 };
