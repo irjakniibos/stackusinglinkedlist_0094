27b3a6484b2c5eed3c637bf22684ada21e31ce1e
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-10 15:16:49 +0700

    membuat method isEmpty

--- stackusingsingly.cpp
+++ stackusingsingly.cpp
@@ -64,6 +64,11 @@ class stack{
             cout << endl;
         }// return the value of the top node
     }
+
+    // IsEmpty operation: check if the stack is empty
+    bool isEmpty() {
+        return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
+    }
     
 };
 
