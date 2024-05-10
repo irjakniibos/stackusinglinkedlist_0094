e6188bcfbac08c3f15482195ce3306907afc9a63
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-10 15:32:23 +0700

    membuat case 3 didalam fungsi utama

--- stackusingsingly.cpp
+++ stackusingsingly.cpp
@@ -101,6 +101,14 @@ int main(){
         cout << "Stack is empty. Cannot pop." << endl;
         }
         break;
+    case 3:
+        if (!stack.isEmpty()) {
+        stack.peek(); // Get the value of the top element
+        }
+        else {
+        cout << "Stack is empty. No top value" << endl;
+        }
+        break;    
     default:
         break;
     }
